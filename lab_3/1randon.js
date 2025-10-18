4ffede8ee0a05790a6c94c791eda9cc5e51a49d7
Вікторія Дмитрієнко <dmytrienkoviktoria0408@gmail.com>
2025-10-18 17:02:33 +0300

    Create 1randon.js

---
+++ lab_3/1randon.js
@@ -0,0 +1,11 @@
+"use strict";
+const random = (min, max) => {
+  if (max === undefined) {
+    max = min;
+    min = 0;
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+console.log(random(10, 20));
+console.log(random(5));
+module.exports = { random };
