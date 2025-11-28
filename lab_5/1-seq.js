46c10b18bba217c5c67be141187d777966596511
Вікторія Дмитрієнко <dmytrienkoviktoria0408@gmail.com>
2025-11-28 19:00:56 +0200

    Create 1-seq.js

---
+++ lab_5/1-seq.js
@@ -0,0 +1,6 @@
+"use strict";
+const seq = (f) => (g) => typeof g === "number" ? f(g) : seq((x) => f(g(x)));
+console.log(seq(x => x + 7)(x => x * 2)(5));
+console.log(seq(x => x * 2)(x => x + 7)(5));
+console.log(seq(x => x + 1)(x => x * 2)(x => x / 3)(x => x - 4)(7));
+module.exports = { seq };
