212fa37a746e824abcb1f47f9b7db0553318cf6a
Вікторія Дмитрієнко <dmytrienkoviktoria0408@gmail.com>
2025-11-28 19:50:19 +0200

    Create 3-wrapper.js

---
+++ lab_8/3-wrapper.js
@@ -0,0 +1,24 @@
+'use strict';
+const contract = (fn, ...types) => {
+  return function(...args) {
+    for (let i = 0; i < args.length; i++) {
+      if (typeof args[i] !== types[i].name.toLowerCase()) {
+        throw new TypeError("Type error in argument " + (i + 1));
+      }
+    }
+    const result = fn(...args);
+    if (typeof result !== types[types.length - 1].name.toLowerCase()) {
+      throw new TypeError("Type error in return value");
+    }
+    return result;
+  };
+};
+const add = (a, b) => a + b;
+const addNumbers = contract(add, Number, Number, Number);
+const res1 = addNumbers(2, 3);
+console.dir(res1);
+const concat = (s1, s2) => s1 + s2;
+const concatStrings = contract(concat, String, String, String);
+const res2 = concatStrings('Hello ', 'world!');
+console.dir(res2);
+module.exports = { contract };
